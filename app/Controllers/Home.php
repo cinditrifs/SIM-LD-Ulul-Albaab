cb28a6dead0a8c60c6ef26555c2250025b33faa5
Nilatil Moena <56217264+nilamoena@users.noreply.github.com>
2020-12-06 18:18:14 +0700

    Update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,39 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('login_admin');
+		return view('main/index');
+	}
+	public function prodak()
+	{
+		return view('main/prodak');
+	}
+	public function artikel()
+	{
+		return view('main/artikel');
+	}
+	public function isiartikel()
+	{
+		return view('main/isiartikel');
+	}
+	public function sekret()
+	{
+		return view('main/sekret');
+	}
+	public function surat()
+	{
+		return view('main/surat');
+	}
+	public function presensi()
+	{
+		return view('main/presensi');
+	}
+	public function syuro()
+	{
+		return view('main/syuro');
+	}
+	public function lpj()
+	{
+		return view('main/lpj');
 	}
 
 	//--------------------------------------------------------------------
